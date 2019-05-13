94002c8d8bfe0292d530b018eb892d3fef5470a1
NipunPC <chiransachintha1994@gmail.com>
2019-05-14 00:59:21 +0530

    brute force method

---
+++ Daily_Coding_problems/problem_#4.py
@@ -0,0 +1,31 @@
+'''
+
+Given an array of integers, find the first missing positive integer in linear time and constant space.
+In other words, find the lowest positive integer that does not exist in the array.
+The array can contain duplicates and negative numbers as well.
+
+'''
+
+'BRUTE FORCE METHOD'
+
+def minPositiveInt(array):
+    n= len(array)
+    minValue = 0
+    for i in array:
+        if i > 0 and minValue == 0:
+            minValue = i
+        elif i > 0 and i <= minValue:
+            minValue = i
+    return minValue
+print(minPositiveInt([1, 2, 0]))
+
+def minValNotInArray(minValue,array):
+    if minValue > 1 or minValue == 0 :
+        return 1
+    else:
+        for i in range(2, minValue+len(array)):
+            if i not in array:
+                return i
+array = [1, 2, 0]
+minValue = minPositiveInt(array)
+print(minValNotInArray(minValue, array))
